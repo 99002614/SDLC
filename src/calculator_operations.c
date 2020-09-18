53c4ed09905f748608b836f6fe816093a8189c13
99002614 <71481122+99002614@users.noreply.github.com>
2020-09-18 22:52:26 +0530

    Update calculator_operations.c

--- src/calculator_operations.c
+++ src/calculator_operations.c
@@ -22,3 +22,30 @@ int divide(int operand1, int operand2)
     else
         return operand1 / operand2;
 }
+int modulus(int operand1, int operand2)
+{
+    return operand1 % operand2;
+}
+int primenumber(int operand1)
+    {
+        int i,count=0;
+        if(operand1<0)
+        {
+            return -1;
+        }
+        for(i=1;i<=operand1;i++)
+        {
+            if(operand1%i==0)
+            {
+                count++;
+            }
+        }
+        if(count==2)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
