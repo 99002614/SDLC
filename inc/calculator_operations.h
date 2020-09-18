443bc695b6268383322bd139387d4eac90bad243
99002614 <71481122+99002614@users.noreply.github.com>
2020-09-19 01:46:51 +0530

    Update calculator_operations.h

--- inc/calculator_operations.h
+++ inc/calculator_operations.h
@@ -112,4 +112,22 @@ int or(int operand1, int operand2);
 * @return Result of the the NOT operation
 */
 int not(int operand1);
+
+
+/**
+*  provides NAND of operand1, operand2 and returns the result
+* @param[in] operand1 
+* @param[in] operand2 
+* @return Result of the the NAND operation
+*/
+int nand(int operand1, int operand2);
+
+
+/**
+*  provides NOR of operand1, operand2 and returns the result
+* @param[in] operand1 
+* @param[in] operand2 
+* @return Result of the the NOR operation
+*/
+int nor(int operand1, int operand2);
 #endif  /* #define __CALCULATOR_OPERATIONS_H__ */
