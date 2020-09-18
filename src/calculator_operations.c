f219e5364f325ae07dca18f47119615d20766d02
99002614 <71481122+99002614@users.noreply.github.com>
2020-09-19 01:46:47 +0530

    Update calculator_operations.c

--- src/calculator_operations.c
+++ src/calculator_operations.c
@@ -84,24 +84,24 @@ int and(int operand1, int operand2)
     int result;
     if(operand1<0 && operand2<0)
     return -1;
-    else if(operand1>0 && operand2>0)
+    else if(operand1>1 && operand2>1)
     return 1;
-    else if(operand1==0 || operand2==0)
-    return 0;
+    else if(operand1==1 && operand2==1)
+    return 1;
     else
-    return 1;
+    return 0;
 }
 int or(int operand1, int operand2)
 {
     int result;
     if(operand1<0 && operand2<0)
     return -1;
-    else if(operand1>0 && operand2>0)
+    else if(operand1>1 && operand2>1)
     return 1;
-    else if(operand1==1 || operand2==1)
-    return 1;
-    else
+    else if(operand1==0 && operand2==0)
     return 0;
+    else
+    return 1;
 }
 int not(int operand1)
 {
@@ -110,4 +110,28 @@ int not(int operand1)
     else
         return 0;
 }
+int nand(int operand1, int operand2)
+{
+    int result;
+    if(operand1<0 && operand2<0)
+    return -1;
+    else if(operand1>1 && operand2>1)
+    return 0;
+    else if(operand1==1 && operand2==1)
+    return 0;
+    else
+    return 1;
+}
+int nor(int operand1, int operand2)
+{
+    if(operand1<0 && operand2<0)
+    return -1;
+    else if(operand1>1 && operand2>1)
+    return 0;
+    else if(operand1==0 && operand2==0)
+    return 1;
+    else
+    return 0;
+}
+
 
